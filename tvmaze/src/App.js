45ed9c3f9c46efbe7d5304f19763f4b96561fc95
Abhijit Mohan Lamb <106537258+abhijitlamb03@users.noreply.github.com>
2022-10-19 13:01:24 +0530

    s
    s

---
+++ tvmaze/src/App.js
@@ -0,0 +1,13 @@
+import NavBar from './Components/NavBar';
+import Search from './Components/Search';
+function App() {
+  return (
+    <>
+      <NavBar />
+      <br/>
+      <Search />
+    </>
+  );
+}
+
+export default App;
