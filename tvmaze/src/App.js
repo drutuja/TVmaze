96abdcb11a520525ac95fe6f7a638029dacf89ce
Abhijit Mohan Lamb <106537258+abhijitlamb03@users.noreply.github.com>
2022-11-09 19:36:43 +0530

    Update App.js

--- tvmaze/src/App.js
+++ tvmaze/src/App.js
@@ -6,7 +6,9 @@ function App() {
       <NavBar />
       <br/>
       <Search />
+      <img src='https://cdn.pixabay.com/photo/2012/08/27/22/59/movie-projector-55122_960_720.png' />
     </>
+    
   );
 }
 
